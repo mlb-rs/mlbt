6efbfba6f3e6321503c562edcf29b58ba999c190
Andrew Schneider <andjschneider@gmail.com>
2026-08-30 20:08:51 -0700

    Use qualified stats as default

--- src/components/stats/table.rs
+++ src/components/stats/table.rs
@@ -32,7 +32,7 @@ impl Default for StatType {
         Self {
             group: StatGroup::Hitting,
             team_player: TeamOrPlayer::Player,
-            qualification: Qualification::All,
+            qualification: Qualification::Qualified,
         }
     }
 }
